d3202eedc1eba5098c2322de81ef2d7583d3e05f
Eevee-17 <99231914+Eevee-17@users.noreply.github.com>
2025-03-01 15:56:08 -0600

    Added game over

--- scripts/main/main.gd
+++ scripts/main/main.gd
@@ -15,33 +15,37 @@ func start():
 func _ready() -> void:
 	$MainControl.hide()
 	$InBetween.hide()
+	$GameOver.hide()
 
 func cutscene():
-	$InBetween.show()
-	
 	lives = $Game.lives
 	money = $Game.money
 	
-	if lives < 4:
-		$InBetween/Hearts/Heart4.hide()
-	if lives < 3:
-		$InBetween/Hearts/Heart3.hide()
-	if lives < 2:
-		$InBetween/Hearts/Heart2.hide()
-	if lives < 1:
-		$InBetween/Hearts/Heart1.hide()
-	
-	$InBetween/MoneyDisplay/MoneyAmount.text = str(money)
-	
-	$InBetween/Label.text = "Steal from the rich businessman!"
-	
-	for num in range(5):
-		$InBetween/Countdown.text = "Countdown: " + str(5 - num) 
-		await get_tree().create_timer(1.0).timeout
-	
-	$Game.start_game($MainControl.game1_scene, 0)
-	
-	$InBetween.hide()
+	if lives > 0:
+		if lives < 4:
+			$InBetween/Hearts/Heart4.hide()
+		if lives < 3:
+			$InBetween/Hearts/Heart3.hide()
+		if lives < 2:
+			$InBetween/Hearts/Heart2.hide()
+		if lives < 1:
+			$InBetween/Hearts/Heart1.hide()
+		
+		$InBetween.show()
+		
+		$InBetween/MoneyDisplay/MoneyAmount.text = str(money)
+		
+		$InBetween/Label.text = "Steal from the rich businessman!"
+		
+		for num in range(3):
+			$InBetween/Countdown.text = "Countdown: " + str(3 - num) 
+			await get_tree().create_timer(1.0).timeout
+		
+		$Game.start_game($MainControl.game1_scene, 0)
+		
+		$InBetween.hide()
+	else:
+		$GameOver.show()
 
 func _on_game_game_ended(game_num: Variant, money_val: Variant, lives_left: Variant) -> void:
 	cutscene()
