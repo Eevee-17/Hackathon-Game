90e67d2e05bbc8b18994d6d3051f084935f06996
Eevee-17 <99231914+Eevee-17@users.noreply.github.com>
2025-03-01 21:16:04 -0600

    Fixed previous change that broke timing in the hand it out minigame

--- scripts/handitout1/hand_it_out_1.gd
+++ scripts/handitout1/hand_it_out_1.gd
@@ -11,7 +11,7 @@ func start_game(money_val):
 
 func _on_player_give(is_business_man: Variant) -> void:
 	if is_business_man:
-		#await get_tree().create_timer(1.0).timeout
+		await get_tree().create_timer(1.0).timeout
 		get_parent().lost_game(-start_money)
 	else:
 		money -= 1
