cea4eef4feb49c8808bed5a90e602cec6a959aac
Eevee-17 <99231914+Eevee-17@users.noreply.github.com>
2025-03-07 18:42:54 -0600

    Changed input for game3 to be more consistant with other games

--- scripts/game3/character_body_2d.gd
+++ scripts/game3/character_body_2d.gd
@@ -20,7 +20,7 @@ func _physics_process(delta: float) -> void:
 	
 	if not has_won:
 		# Handle jump.
-		if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+		if Input.is_action_just_pressed("space") and is_on_floor():
 			velocity.y = JUMP_VELOCITY-40
 			
 		# Get the input direction and handle the movement/deceleration.
